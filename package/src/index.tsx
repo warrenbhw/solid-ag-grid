20bc3667e9985fe84173fd9c0e27d53a4996187b
Ben Warren <warrenbhw@gmail.com>
2024-08-14 00:19:07 -0700

    remove junk

--- package/src/index.tsx
+++ package/src/index.tsx
@@ -3,11 +3,6 @@ export default AgGridSolid;
 
 export * from "./grid/agGridSolid";
 
-// Just for basic keepalive testing
-export function isEven(n: number) {
-  return n % 2 === 0;
-}
-
 // /* @refresh reload */
 // import { render } from 'solid-js/web';
 //
